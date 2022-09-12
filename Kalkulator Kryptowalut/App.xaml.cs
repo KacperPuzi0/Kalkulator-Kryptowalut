b53e8cfbc2f863bc574acb732c02213b8ad0f778
KacperPuzi0 <79982696+KacperPuzi0@users.noreply.github.com>
2022-09-12 17:04:01 +0200

    ++

--- Kalkulator Kryptowalut/App.xaml.cs
+++ Kalkulator Kryptowalut/App.xaml.cs
@@ -6,12 +6,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace Kalkulator_Kryptowalut
+namespace Projekt_koncowy
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
+        
+
     }
 }
